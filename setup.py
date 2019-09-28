e3839414c6f6ff4bfd942d84a9317f06735bc955
Gregory Lindsey <gclindsey@gmail.com>
2019-09-28 14:37:16 -0700

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='An EDA of building permits in Los Angeles.',
+    author='Gregory Lindsey',
+    license='MIT',
+)
