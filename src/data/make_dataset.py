a6c74da0573749d864522f622a34dcf2f2492877
Gregory Lindsey <gclindsey@gmail.com>
2019-09-29 23:58:33 -0700

    Fix column names and write CSV

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,34 +1,4 @@
 # -*- coding: utf-8 -*-
-import click
-import logging
-from pathlib import Path
-from dotenv import find_dotenv, load_dotenv
-
-
-@click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
-    """ Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
-    """
-    logger = logging.getLogger(__name__)
-    logger.info('making final data set from raw data')
-
-
-if __name__ == '__main__':
-    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    logging.basicConfig(level=logging.INFO, format=log_fmt)
-
-    # not used in this stub but often useful for finding various files
-    project_dir = Path(__file__).resolve().parents[2]
-
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
-
-    main()
-
 import numpy as np
 import pandas as pd
 import re
@@ -37,7 +7,6 @@ import seaborn as sns
 plt.style.use('ggplot')
 
 import os
-print(os.listdir('..'))
 import sys
 
 from urllib.request import urlretrieve
@@ -46,7 +15,39 @@ filename = 'Building_and_Safety_Permit_Information.csv'
 url = 'https://data.lacity.org/api/views/yv23-pmwf/rows.csv'
 
 # Check if dataset is presemt
-if filename not in os.listdir('../data/raw/'):
+if filename not in os.listdir('./data/raw/'):
+    print("downloading...")
     url = 'https://data.lacity.org/api/views/yv23-pmwf/rows.csv'
     #urlretrieve(url, filename)
-    print(os.listdir('../data/raw/'))
+    print(os.listdir('./data/raw/'))
+
+# Import CSV
+df = pd.read_csv('./data/raw/'+filename, parse_dates=["Issue Date", "Status Date"])
+df.index.names = ['Index']
+
+# Replace whitespace with underscore
+df.columns = df.columns.str.replace(' ', '_')
+
+# Replace hyphen with underscore
+df.columns = df.columns.str.replace('-', '_')
+
+# Replace hashtag with No (short for number)
+df.columns = df.columns.str.replace('#', 'No')
+
+# Replace forward slash with underscore
+df.columns = df.columns.str.replace('/', '_')
+
+# Remove period
+df.columns = df.columns.str.replace('.', '')
+
+# Remove open parenthesis
+df.columns = df.columns.str.replace('(', '')
+
+# Remove closed parenthesis
+df.columns = df.columns.str.replace(')', '')
+
+# Remove apostrophe
+df.columns = df.columns.str.replace("'", '')
+
+# Write out to disk
+df.to_csv('./data/interim/'+filename)
